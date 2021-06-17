5f03adb47649c957723eb1ac6ab7bab79132ef29
makam-santhosh-au9 <santosmakam@gmail.com>
2021-06-17 18:13:14 +0530

    FInalProject

--- FInalProject/Backend/controllers/PostController.js
+++ FInalProject/Backend/controllers/PostController.js
@@ -124,3 +124,14 @@ exports.deletePost= (req, res)=>{
         });
     });
 };
+
+
+exports.Photo =(req,res,next )=>{
+    res.set("Content-Type",req.post.photo.contentType)
+    return res.send(req.post.photo.data);
+}
+
+
+exports.singlePost = (req, res) => {
+    return res.json(req.post);
+};
--- FInalProject/Backend/routes/post.js
+++ FInalProject/Backend/routes/post.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const { requireSignin } = require('../controllers/auth');
 const Postcontroller = require("../controllers/PostController");
+const {Photo}= require("../controllers/PostController");
 const { userById } = require('../controllers/userController');
-const validators = require("../validators/index")
+const validators = require("../validators/index");
+const {singlePost} = require('../controllers/PostController')
 
 
 
 const router = express.Router();
+
 router.get('/posts',Postcontroller.Getposts);
 router.post('/post/new/:userId',requireSignin,Postcontroller.CreatePost,validators.createPostValidators);
 router.get('/posts/by/:userId',requireSignin, Postcontroller.postsByUser);
-router.put('/post/:postId', requireSignin, Postcontroller.isPoster,Postcontroller.updatePost );
+router.get('/post/:postId', singlePost);
+router.put("/post/:postId", requireSignin, Postcontroller.isPoster,Postcontroller.updatePost );
 router.delete('/post/:postId', requireSignin, Postcontroller.isPoster, Postcontroller.deletePost );
-
+//photo
+router.get("/post/photo/:postId", Photo)
 
 
 // any route containing :userId, in app will first executes userById().
